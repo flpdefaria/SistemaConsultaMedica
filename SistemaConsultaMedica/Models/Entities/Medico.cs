ee0c06331e6e32332f067d2bb7f14d0acc91b492
Felipe Faria <felipe.faria@azurebrasil.cloud>
2025-03-25 14:35:35 -0300

    Create entities medico

---
+++ SistemaConsultaMedica/Models/Entities/Medico.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemaConsultaMedica.Models.Entities
+{
+    public class Medico
+    {
+        public int Id { get; set; }
+        public string CRM { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+}
