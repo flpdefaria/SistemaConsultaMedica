ef226402117dfa4e2f78a6301ebc8f13014530fe
Felipe Faria <afariaflp@gmail.com>
2025-03-26 17:59:06 -0300

    Remove Speciality

--- SistemaConsultaMedica/Models/Entities/Medico.cs
+++ SistemaConsultaMedica/Models/Entities/Medico.cs
@@ -5,6 +5,5 @@
         public int Id { get; set; }
         public string CRM { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
-        public string Speciality { get; set; } = string.Empty;
     }
 }
