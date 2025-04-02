1cd3b92ab3bd402517abbfc6a5376cd65ec31973
Felipe Faria <afariaflp@gmail.com>
2025-04-02 20:46:53 -0300

    Add connectstring

---
+++ SistemaConsultaMedica/Models/Contexts/SisMedContext.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaConsultaMedica.Models.Entities;
+using SistemaConsultaMedica.Models.EntityConfigurations;
+
+namespace SistemaConsultaMedica.Models.Contexts;
+
+public class SisMedContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+
+    public SisMedContext(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public DbSet<Medico> Medicos => Set<Medico>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_configuration.GetConnectionString("SisMed"));
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new MedicoConfiguration());
+    }
+}
