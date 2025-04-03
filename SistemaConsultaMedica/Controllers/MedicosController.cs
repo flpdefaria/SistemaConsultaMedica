e359fb97f4f7fc14340311b89d29e104fc75fdf0
Felipe Faria <afariaflp@gmail.com>
2025-04-02 22:20:25 -0300

    Create list of doctor

--- SistemaConsultaMedica/Controllers/MedicosController.cs
+++ SistemaConsultaMedica/Controllers/MedicosController.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
+using SistemaConsultaMedica.Models.Contexts;
+using SistemaConsultaMedica.ViewModels.Medicos;
 
 namespace SistemaConsultaMedica.Controllers;
 
 public class MedicosController : Controller
 {
     // GET
+    private readonly SisMedContext _context;
+
+    public MedicosController(SisMedContext context)
+    {
+        _context = context;
+    }
     public IActionResult Index()
     {
-        return View();
+        var medicos = _context.Medicos.Select(x => new ListarMedicoViewModel
+        {
+            Id = x.Id,
+            CRM = x.CRM,
+            Name = x.Name
+        });
+        
+        return View(medicos);
     }
 }
--- SistemaConsultaMedica/Program.cs
+++ SistemaConsultaMedica/Program.cs
@@ -1,7 +1,10 @@
+using SistemaConsultaMedica.Models.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<SisMedContext>();
 
 var app = builder.Build();
 
