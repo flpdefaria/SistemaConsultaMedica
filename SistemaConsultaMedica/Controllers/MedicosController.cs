61d46c72af871e0f199560c963579123fa838ac6
Felipe Faria <afariaflp@gmail.com>
2025-04-02 23:30:23 -0300

    Create ListarMedicoViewModel and filter in Index of medicosController

--- SistemaConsultaMedica/Controllers/MedicosController.cs
+++ SistemaConsultaMedica/Controllers/MedicosController.cs
@@ -13,14 +13,18 @@ public class MedicosController : Controller
     {
         _context = context;
     }
-    public IActionResult Index()
+    public IActionResult Index(string filtro)
     {
-        var medicos = _context.Medicos.Select(x => new ListarMedicoViewModel
+        var medicos = _context.Medicos
+            .Where(x => x.Name.Contains(filtro) || x.CRM.Contains(filtro))
+            .Select(x => new ListarMedicoViewModel
         {
             Id = x.Id,
             CRM = x.CRM,
             Name = x.Name
         });
+        
+        ViewBag.Filtro = filtro;
         
         return View(medicos);
     }
