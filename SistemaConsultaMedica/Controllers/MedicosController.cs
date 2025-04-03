c3ae2362680d354e5370b38880ffaaa42c3e93ee
Felipe Faria <afariaflp@gmail.com>
2025-04-03 00:00:58 -0300

    Add pagination in index medicos

--- SistemaConsultaMedica/Controllers/MedicosController.cs
+++ SistemaConsultaMedica/Controllers/MedicosController.cs
@@ -8,12 +8,12 @@ public class MedicosController : Controller
 {
     // GET
     private readonly SisMedContext _context;
-
+    private const int tamanho_pagina = 10;
     public MedicosController(SisMedContext context)
     {
         _context = context;
     }
-    public IActionResult Index(string filtro)
+    public IActionResult Index(string filtro, int pagina = 1)
     {
         var medicos = _context.Medicos
             .Where(x => x.Name.Contains(filtro) || x.CRM.Contains(filtro))
@@ -25,7 +25,9 @@ public class MedicosController : Controller
         });
         
         ViewBag.Filtro = filtro;
+        ViewBag.NumeroPagina = pagina;
+        ViewBag.TotalPaginas = Math.Ceiling((decimal)medicos.Count() / tamanho_pagina);
         
-        return View(medicos);
+        return View(medicos.Skip((pagina - 1) * tamanho_pagina).Take(tamanho_pagina));
     }
 }
