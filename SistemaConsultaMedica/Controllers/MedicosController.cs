4f815c59e6fbbd3d5c9cbc19360a5186211e0eaf
Felipe Faria <afariaflp@gmail.com>
2025-04-03 23:39:08 -0300

    Create view Edit

--- SistemaConsultaMedica/Controllers/MedicosController.cs
+++ SistemaConsultaMedica/Controllers/MedicosController.cs
@@ -14,27 +14,29 @@ public class MedicosController : Controller
     private readonly SisMedContext _context;
     private readonly IValidator<AdicionarMedicoViewModel> _adicionarMedicoValidator;
     private const int tamanhoPagina = 10;
+
     public MedicosController(SisMedContext context, IValidator<AdicionarMedicoViewModel> adicionarMedicoValidator)
     {
         _context = context;
         _adicionarMedicoValidator = adicionarMedicoValidator;
         ;
     }
+
     public IActionResult Index(string filtro, int pagina = 1)
     {
         var medicos = _context.Medicos
             .Where(x => x.Name.Contains(filtro) || x.CRM.Contains(filtro))
             .Select(x => new ListarMedicoViewModel
-        {
-            Id = x.Id,
-            CRM = x.CRM,
-            Name = x.Name
-        });
-        
+            {
+                Id = x.Id,
+                CRM = x.CRM,
+                Name = x.Name
+            });
+
         ViewBag.Filtro = filtro;
         ViewBag.NumeroPagina = pagina;
         ViewBag.TotalPaginas = Math.Ceiling((decimal)medicos.Count() / tamanhoPagina);
-        
+
         return View(medicos.Skip((pagina - 1) * tamanhoPagina).Take(tamanhoPagina));
     }
 
@@ -42,27 +44,65 @@ public class MedicosController : Controller
     {
         return View();
     }
+
     [HttpPost]
     [ValidateAntiForgeryToken]
     public IActionResult Adicionar(AdicionarMedicoViewModel dados)
     {
         var validacao = _adicionarMedicoValidator.Validate(dados);
-        
+
         if (!validacao.IsValid)
         {
             validacao.AddToModelState(ModelState, string.Empty);
             return View(dados);
         }
-        
+
         var medico = new Medico
         {
             CRM = dados.CRM,
             Name = dados.Name
         };
-        
+
         _context.Medicos.Add(medico);
         _context.SaveChanges();
-        
+
         return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Editar(int id)
+    {
+        var medico = _context.Medicos.Find(id);
+        
+        if (medico != null)
+        {
+            return View(new EditarMedicoViewModel
+            {
+                Id = medico.Id,
+                CRM = medico.CRM,
+                Name = medico.Name
+            });
+        }
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Editar(int id, EditarMedicoViewModel dados)
+    {
+        var medico = _context.Medicos.Find(id);
+
+        if (medico != null)
+        {
+            medico.CRM = dados.CRM;
+            medico.Name = dados.Name;
+
+            _context.Medicos.Update(medico);
+            _context.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+        
+        return NotFound();
     }
 }
