226daf59ad57a0c2757671a9edee373a301733dc
Felipe Faria <afariaflp@gmail.com>
2025-04-09 10:14:01 -0300

    Refactory case pacientes

--- SistemaConsultaMedica/Controllers/PacientesController.cs
+++ SistemaConsultaMedica/Controllers/PacientesController.cs
@@ -7,25 +7,28 @@ namespace SistemaConsultaMedica.Controllers;
 
 public class PacientesController : Controller
 {
-    //GET
     private readonly SisMedContext _context;
-
+    private const int tamanhoPagina = 10;
+    
     public PacientesController(SisMedContext context)
     {
         _context = context;
     }
     
-    public IActionResult Index()
+    public IActionResult Index(string filtro, int pagina = 1)
     {
         var pacientes = _context.Pacientes
-            .Select(x => new ListarPacientesViewModel
+            .Where(x => x.Name.Contains(filtro) || x.CPF.Contains(filtro))
+            .Select(x => new ListarPacienteViewModel
             {
                 Id = x.Id,
                 CPF = x.CPF,
                 Name = x.Name,
-                DataNascimento = x.DataNascimento
             });
         
-        return View(pacientes);
+        ViewBag.Filtro = filtro;
+        ViewBag.NumeroPagina = pagina;
+        ViewBag.TotalPaginas = Math.Ceiling((decimal)pacientes.Count() / tamanhoPagina);
+        return View(pacientes.Skip((pagina - 1) * tamanhoPagina).Take(tamanhoPagina));
     }
 }
--- SistemaConsultaMedica/ViewModels/Pacientes/ListarPacienteViewModel.cs
+++ SistemaConsultaMedica/ViewModels/Pacientes/ListarPacienteViewModel.cs
@@ -1,13 +1,11 @@
 namespace SistemaConsultaMedica.ViewModels.Pacientes;
 
-public class ListarPacientesViewModel
+public class ListarPacienteViewModel
 {
     public int Id { get; set; }
     
     public string CPF { get; set; } = string.Empty;
     
     public string Name { get; set; } = string.Empty;
-    
-    public DateTime DataNascimento { get; set; }
     
 }
