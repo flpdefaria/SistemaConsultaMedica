78563a17082aa5a2ca61efb8413c71b6b00de99b
Felipe Faria <afariaflp@gmail.com>
2025-04-18 19:57:42 -0300

    implementing query list

--- SistemaConsultaMedica/Controllers/ConsultasController.cs
+++ SistemaConsultaMedica/Controllers/ConsultasController.cs
@@ -1,11 +1,39 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaConsultaMedica.Models.Contexts;
+using SistemaConsultaMedica.Models.Enums;
+using SistemaConsultaMedica.ViewModels.Consultas;
 
 namespace SistemaConsultaMedica.Controllers;
 
 public class ConsultasController : Controller
 {
-    public IActionResult Index()
+    private readonly SisMedContext _context;
+    private const int tamanhoPagina = 10;
+
+    public ConsultasController(SisMedContext context)
     {
-        return View();
+        _context = context;
+    }
+
+    public IActionResult Index(string filtro, int pagina = 1)
+    {
+        var consultas = _context.Consultas
+            .Include(c => c.Paciente)
+            .Include(c => c.Medico)
+            .Where(c => c.Paciente.Name.Contains(filtro) || c.Medico.Name.Contains(filtro))
+            .Select(c => new ListarConsultaViewModel
+            {
+                Id = c.Id,
+                Paciente = c.Paciente.Name,
+                Medico = c.Medico.Name,
+                Data = c.Data,
+                Tipo = c.Tipo == TipoConsulta.Eletiva ? "Eletiva" : "Urgencia"
+            });
+
+        ViewBag.NumeroPagina = pagina;
+        ViewBag.TotalPaginas = Math.Ceiling((decimal)consultas.Count() / tamanhoPagina);
+        
+        return View(consultas.Skip((pagina - 1) * tamanhoPagina).Take(tamanhoPagina));
     }
 }
