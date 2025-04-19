7e80e9db20c9257cfc93f5afe23669d9bf9e9e98
Felipe Faria <afariaflp@gmail.com>
2025-04-19 19:51:59 -0300

    Add page of insert consultas

--- SistemaConsultaMedica/Controllers/ConsultasController.cs
+++ SistemaConsultaMedica/Controllers/ConsultasController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SistemaConsultaMedica.Models.Contexts;
 using SistemaConsultaMedica.Models.Enums;
@@ -35,5 +36,18 @@ public class ConsultasController : Controller
         ViewBag.TotalPaginas = Math.Ceiling((decimal)consultas.Count() / tamanhoPagina);
         
         return View(consultas.Skip((pagina - 1) * tamanhoPagina).Take(tamanhoPagina));
+    }
+
+    public IActionResult Adicionar()
+    {
+
+        ViewBag.TiposConsulta = new[]
+        {
+            new SelectListItem { Text = "Eletiva", Value = TipoConsulta.Eletiva.ToString() },
+            new SelectListItem { Text = "Urgência", Value = TipoConsulta.Urgencia.ToString() },
+        };
+        
+        ViewBag.Medicos = _context.Medicos.OrderBy(x => x.Name).Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() });
+        return View();
     }
 }
